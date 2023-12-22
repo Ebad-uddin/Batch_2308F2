234e49e47c3cfcad48fc56dcc39b817f03893c13
Ebad Uddin <ebad@aptech.com>
2023-12-22 19:03:56 +0500

    jquery done till css method

--- jquery/app.js
+++ jquery/app.js
@@ -3,14 +3,35 @@ $(document).ready(function(){
 let para = $('#para');
 let hidebtn = $('#hidebtn');
 let showbtn = $('#showbtn');
+let toggle = $('#toggle');
 
 // console.log(para, btn)
 
 hidebtn.click(function(){
-    para.hide();
+    para.hide(2000, function(){
+        alert("hide method called");
+    });
 })
 showbtn.click(function(){
-    para.show();
+    para.show(3000, function(){
+        console.log("<h1>show method called</h1>");
+    });
+})
+
+toggle.click(function(){
+    para.toggle();
+})
+
+// light and dark mode 
+let box = $('#box');
+let darkbtn = $('#dark');
+let lightbtn = $('#light');
+
+darkbtn.click(function(){
+    box.css({"background-color" : "black", "color" : "white"})
+})
+lightbtn.click(function(){
+    box.css({"background-color" : "white", "color" : "black"})
 })
 
 
